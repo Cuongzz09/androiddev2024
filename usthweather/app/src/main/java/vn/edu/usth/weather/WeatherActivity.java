6cbc5a438507ae6447adae5e878e8474fe0fd03f
Tran Viet Cuong <cuong61595@gmail.com>
2024-09-11 17:28:41 +0700

    practical 5

---
+++ usthweather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,55 @@
+package vn.edu.usth.weather;
+import android.os.Bundle;
+import android.util.Log;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.fragment.app.FragmentManager;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    private static final String TAG = "WeatherActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate is called");
+
+
+        setContentView(R.layout.activity_weather);
+
+
+        // Create a new instance of ForecastFragment
+        ForecastFragment forecastFragment = new ForecastFragment();
+
+        // Add the fragment to the 'container' FrameLayout using dynamic code
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.main, forecastFragment).commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart is called");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume is called");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause is called");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop is called");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy is called");
+    }
+}
