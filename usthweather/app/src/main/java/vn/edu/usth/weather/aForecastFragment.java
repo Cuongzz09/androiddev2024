5e2c3b69e98ef84db202d0d80420cbe365e29747
Cuongzz09 <cuong61595@gmail.com>
2024-10-02 15:41:26 +0700

    Practical 13

--- usthweather/app/src/main/java/vn/edu/usth/weather/aForecastFragment.java
+++ usthweather/app/src/main/java/vn/edu/usth/weather/aForecastFragment.java
@@ -1,20 +1,22 @@
 package vn.edu.usth.weather;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link WeatherAndForecastFragment#newInstance} factory method to
+ * Use the {@link aForecastFragment#newInstance} factory method to
  * create an instance of this fragment.
- *
  */
-public class WeatherAndForecastFragment extends Fragment {
+public class aForecastFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,26 +27,26 @@ public class WeatherAndForecastFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    public aForecastFragment() {
+        // Required empty public constructor
+    }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment WeatherAndForecastFragment.
+     * @return A new instance of fragment aForecastFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static WeatherAndForecastFragment newInstance(String param1, String param2) {
-        WeatherAndForecastFragment fragment = new WeatherAndForecastFragment();
+    public static aForecastFragment newInstance(String param1, String param2) {
+        aForecastFragment fragment = new aForecastFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    public WeatherAndForecastFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -53,23 +55,33 @@ public class WeatherAndForecastFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_weather_and_forecast, container, false);
-
-        Fragment childFragment1 = new aForecastFragment();
-        Fragment childFragment2 = new WeatherFragment();
-        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-        transaction.replace(R.id.weather, childFragment2);
-        transaction.replace(R.id.forecast, childFragment1);
-        transaction.commit();
-        return view;
-        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_weather_and_forecast, container, false);
+//        View v = new View(getContext());
+//        v.setBackgroundColor(0x20FF0000);
+//
+//        LinearLayout LL = new LinearLayout(getContext());
+//        LL.setOrientation(LinearLayout.VERTICAL);
+//
+////        ImageView sunny = new ImageView(getContext());
+////        sunny.setImageResource(R.drawable.sunny);
+//        ImageView cloudy = new ImageView(getContext());
+//        cloudy.setImageResource(R.drawable.cloudynew);
+//
+//        TextView text = new TextView(getContext());
+//        text.setText("today:");
+//        text.setTextSize(30);
+//        LL.addView(text);
+////      LL.addView(sunny);
+//        LL.addView(cloudy);
+//        LL.addView(v);
+//        return LL;
+        // Inflate the layout for this fragment(this is real comment)
+      return inflater.inflate(R.layout.fragment_a_forecast, container, false);
     }
 }
+
